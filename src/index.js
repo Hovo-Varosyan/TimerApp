c1c2bc61d273f4651428c07f08e0b36aeb565d29
Hovo <hovhannes.varosyan.02@mail.ru>
2023-10-13 01:52:41 +0400

    bux fixed and upgrad app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const url = require('url');
-const { app, BrowserWindow, screen } = require('electron');
-
+const { app, BrowserWindow, screen, globalShortcut } = require('electron');
+let os = require('os')
 let win;
 
 function createWindow() {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
 
     win = new BrowserWindow({
         width: width,
@@ -17,17 +18,57 @@ function createWindow() {
         protocol: 'file',
         slashes: true
     }));
-
+    win.maximize()
     win.setMenu(null);
-    win.on('minimize', (event) => {
-        event.preventDefault(); 
-        win.hide(); 
-      });
+
+
+
+
+
+    win.on('resize', () => {
+
+
+        try {
+
+            WindowSize()
+
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+
+    }
+    );
+    win.webContents.setBackgroundThrottling(false);
+
+
     win.on('closed', () => {
-    win = null;
-});
+        win = null;
+    });
 
 }
 
+function WindowSize() {
+    const [x, y] = win.getPosition()
 
+    const width = Math.round((screen.getPrimaryDisplay().size.width * 20) / 100) < 350 ? 350 : Math.round((screen.getPrimaryDisplay().size.width * 20) / 100)
+    const height = width
+    if (os.platform !== "darwin") {
+        win.setBounds({ x, y, width, height })
+    }
+    else {
+        if (!x >= 45) {
+            x = 45
+
+        }
+        if (y >= 45) {
+            y = 45
+        }
+        win.setBounds({ x, y, width, height })
+    }
+
+
+    win.setAlwaysOnTop(true, 'normal', 'bellow');
+}
 app.on("ready", createWindow);
