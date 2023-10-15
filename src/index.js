e2e49a72226695c4462d326d06d2992909ebc476
Hovo <hovhannes.varosyan.02@mail.ru>
2023-10-16 03:05:30 +0400

    test update forms

--- src/index.js
+++ src/index.js
@@ -47,42 +47,6 @@ function createWindow() {
             createWindow()
         }
     });
-
-    win.on(`closed`, () => {
-        win = null;
-    });
-}
-
-function WindowSize() {
-    const [x, y] = win.getPosition()
-
-    const width = Math.round((screen.getPrimaryDisplay().size.width * 20) / 100) < 350 ? 350 : Math.round((screen.getPrimaryDisplay().size.width * 20) / 100)
-    const height = width
-    if (os.platform() !== `darwin`) {
-        win.setBounds({ x, y, width, height })
-    }
-    else {
-        if (!x >= 45) {
-            x = 45
-
-        }
-        if (y >= 45) {
-            y = 45
-        }
-        win.setBounds({ x, y, width, height })
-    }
-
-
-    win.setAlwaysOnTop(true, `normal`, `bellow`);
-}
-
-app.on(`ready`, () => {
-   
-    createWindow()
-    autoUpdater.autoDownload = false;
-    autoUpdater.allowPrerelease = true;
-    autoUpdater.devMode = true;
-    autoUpdater.checkForUpdates();
     autoUpdater.on(`update-available`, (_event) => {
 
         const dialogOpts = {
@@ -130,6 +94,43 @@ app.on(`ready`, () => {
         };
         dialog.showMessageBox(dialogOpts)
     });
+
+    win.on(`closed`, () => {
+        win = null;
+    });
+}
+
+function WindowSize() {
+    const [x, y] = win.getPosition()
+
+    const width = Math.round((screen.getPrimaryDisplay().size.width * 20) / 100) < 350 ? 350 : Math.round((screen.getPrimaryDisplay().size.width * 20) / 100)
+    const height = width
+    if (os.platform() !== `darwin`) {
+        win.setBounds({ x, y, width, height })
+    }
+    else {
+        if (!x >= 45) {
+            x = 45
+
+        }
+        if (y >= 45) {
+            y = 45
+        }
+        win.setBounds({ x, y, width, height })
+    }
+
+
+    win.setAlwaysOnTop(true, `normal`, `bellow`);
+}
+
+app.on(`ready`, () => {
+   
+    createWindow()
+    autoUpdater.autoDownload = false;
+    autoUpdater.allowPrerelease = true;
+    autoUpdater.devMode = true;
+    autoUpdater.checkForUpdates();
+    
 });
 
 
